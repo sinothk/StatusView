7ead17811bfd42d03cd1fa6efafac73a3be4d1ab
SINOTHK\LiangYT <381518188@qq.com>
2018-01-25 16:31:27 +0800

    2018年1月25日 16:31:24

---
+++ StatusView/src/main/java/com/sinothk/view/status/OnShowHideViewListener.java
@@ -0,0 +1,13 @@
+package com.sinothk.view.status;
+
+import android.view.View;
+
+/**
+ * Created by chenpengfei on 2016/12/15.
+ */
+public interface OnShowHideViewListener {
+
+    void onShowView(View view, int id);
+
+    void onHideView(View view, int id);
+}
---
+++ app/src/main/java/com/sinothk/view/statusDemo/MyAdapter.java
@@ -0,0 +1,36 @@
+package com.sinothk.view.statusDemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * <pre>
+ *  @author 梁玉涛 (https://github.com/sinothk)
+ *  @Create 2018/1/25 16:23
+ *  @Project: StatusViewLib
+ *  @Description:
+ *  @Update:
+ * <pre>
+ */
+class MyAdapter extends RecyclerView.Adapter {
+
+    public MyAdapter(OneStepActivity oneStepActivity, List<Message> messageList) {
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
